c98a87fa485a5b52b8dd4eda642145146d9556b7
aistoc <andreea_istoc@yahoo.com>
2020-11-10 15:33:38 +0200

    webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: ['regenerator-runtime/runtime', './index.js'],
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
