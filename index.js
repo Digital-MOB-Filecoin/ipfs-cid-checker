6b83292ee4b6bf20faa3d97c9f1d2fa7fcd5db20
aistoc <andreea_istoc@yahoo.com>
2020-11-12 20:03:19 +0200

    Use test file

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import Block from 'ipld-block';
 import Repo from 'ipfs-repo';
 import last from 'it-last';
 import BlockService from 'ipfs-block-service';
-import getWorkingCID from './CidThatWorks.js';
-import getNotWorkingCID from './CidThatFails.js';
 import * as fs from 'fs';
-import textEncoding from 'text-encoding';
 
+function getFileSize(file) {
+  let stats = fs.statSync(file);
+  return stats.size;
+}
 
 // The purpose of this app is to expose a problem in 
 // the IPFS importer/exporter, related to rawLeaves = false (CIDs with codec != 'raw').
@@ -25,18 +26,16 @@ import textEncoding from 'text-encoding';
   let ipld = new Ipld({ blockService: blockService });
   console.log('IPFS repo initialized');
   
-  // test with an working CID: bafk2bzacebhlhbcnhmvover42qq5bx773c522skieho6nhtbz7d2ow3f4sw24
-  // ** uncomment the following lines to test **
-  //let importedData = new Buffer(getWorkingCID(), 'base64');
-  //let rawLeavesOption = true;
-  
-  // test with a CID that fails: bafykbzaceb7xzlm65omwugcavfxxcxclhnwamjbcodz5rz4kbyhenq7m4pivg
-  let importedData = new Buffer(getNotWorkingCID(), 'base64');
   let rawLeavesOption = false;
+
+  const source = [{
+    path: 'testfile',
+    content: fs.createReadStream('testfile')
+  }]
 
   const entry = await last(
     importer(
-      [{ content: importedData }],
+      source,
       {
         put: async (data, { cid }) => {
           const block = new Block(data, cid);
@@ -51,60 +50,24 @@ import textEncoding from 'text-encoding';
     ),
   );
 
-  // at this point, by using the notWorkingCID, the result of the import will show a different CID name
   console.log('Import results:');
   console.log(entry);
 
   // try to export the bytes previously pushed
   const result = await exporter(entry.cid, ipld);
 
-  // see the result of the export
-  // console.log('Exported: ' + JSON.stringify(result));
-
-  // stream the content of unixfs node after export
-  console.log('importedData size: ' + importedData.length)
-  const size = entry.size;
-  console.log('entry.size: ' + size);
-  
   let exportedBytes = Buffer.from("");
   for await (const buf of result.content()) {
     exportedBytes = Buffer.concat([exportedBytes, buf]);
   }
-  // log the content of unixfs node after export
-  console.log('exportedBytes size: ' + exportedBytes.length);
-  console.log(exportedBytes);
 
   // write it to a file
-  fs.writeFileSync("Exported" + result.name + ".txt", exportedBytes);
+  fs.writeFileSync("testfile.export", exportedBytes);
 
-  var exportedData = new textEncoding.TextDecoder("utf-8").decode(exportedBytes);
+  console.log('imported file size: ', getFileSize('testfile'));
+  console.log('entry.size: ' + entry.size);
+  console.log('exported file size: ', getFileSize('testfile.export'));
+
   
-  // compare the bytes from unixfs node with 
-  // console.log('importedData = ' + importedData);
-  // console.log('exportedData = ' + exportedData);
-  if (importedData == exportedData) {
-    console.log('Success');
-  } else {
-    console.log('Failed');
-  }
-  
+
 })();
-
-/*
-Notice the padded bytes after export:
-
--> last bytes of CID that fails (base64 encoded):
-W8NK8w8YxDdlzb11SE0nKsu0za4HF7UMYtf+znddUnJOvArTaZ8hpwpnDDxwIgQD9yzJ/TsZTb0qIjU9qvq5CKK
-
--> last bytes of CID that fails (base64 encoded), after being exported:
-W8NK8w8YxDdlzb11SE0nKsu0za4HF7UMYtf+znddUnJOvArTaZ8hpwpnDDxwIgQD9yzJ/TsZTb0qIjU9qvq5CKK
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==
-*/
